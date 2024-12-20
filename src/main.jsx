5aa6c8f851584a4137ecd535ad56031b0e7e8c10
dazzlerabhi30800 <abhisingh30800@gmail.com>
2024-12-20 20:16:44 +0530

    added signInFallbackUrl in clerk provider

--- src/main.jsx
+++ src/main.jsx
@@ -13,13 +13,13 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
   throw new Error("Add your Clerk Publishable Key to the .env.local file");
 }
-console.log(PUBLISHABLE_KEY);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider
       publishableKey={PUBLISHABLE_KEY}
-      signInFallbackRedirectUrl={"/feed"}
+      signInFallbackRedirectUrl="/feed"
+      signUpFallbackRedirectUrl="/feed"
       afterSignOutUrl="/"
     >
       <BrowserRouter>
