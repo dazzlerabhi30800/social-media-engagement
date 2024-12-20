54e7a717aeb19cc61083277080f9892e7884b6c4
dazzlerabhi30800 <abhisingh30800@gmail.com>
2024-12-20 19:54:59 +0530

    just checking some bug not working

--- src/main.jsx
+++ src/main.jsx
@@ -13,15 +13,20 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
   throw new Error("Add your Clerk Publishable Key to the .env.local file");
 }
+console.log(PUBLISHABLE_KEY);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <ClerkProvider
+        publishableKey={PUBLISHABLE_KEY}
+        signInFallbackRedirectUrl={"/feed"}
+        afterSignOutUrl="/"
+      >
         <SocialContextProvider>
           <App />
         </SocialContextProvider>
       </ClerkProvider>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
