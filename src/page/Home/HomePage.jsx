23cfb5550dd115f02d3177ba3d703d9220237eaa
dazzlerabhi30800 <abhisingh30800@gmail.com>
2024-12-21 11:34:22 +0530

    added custom google signin button

--- src/page/Home/HomePage.jsx
+++ src/page/Home/HomePage.jsx
@@ -1,6 +1,16 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { useSignIn } from "@clerk/clerk-react";
 
 const Home = () => {
+  const { signIn, isLoaded } = useSignIn();
+  const signInWithGoogle = () => {
+    if (!isLoaded) return;
+    signIn.authenticateWithRedirect({
+      strategy: "oauth_google",
+      redirectUrl: "/sso-callback",
+      redirectUrlComplete: "/",
+    });
+  };
   return (
     <div className="h-screen relative bg-[url('/banner.png')] bg-center bg-cover md:bg-[length:100%_180%] md:bg-no-repeat w-full bg-red-200 text-darkGrey">
       {/* Google Signin Comp */}
@@ -18,13 +28,20 @@ const Home = () => {
             Moments That Matter, Shared Forever.
           </p>
         </div>
-        <Link
-          to="/sign-in"
+        {/* <Link */}
+        {/*   to="/sign-in" */}
+        {/*   className="flex items-center gap-3 bg-darkGrey rounded-[30px] p-3 px-8 text-white font-semibold hover:brightness-125" */}
+        {/* > */}
+        {/*   <img src="./google.png" alt="google" /> */}
+        {/*   Continue with Google */}
+        {/* </Link> */}
+        <button
+          onClick={signInWithGoogle}
           className="flex items-center gap-3 bg-darkGrey rounded-[30px] p-3 px-8 text-white font-semibold hover:brightness-125"
         >
           <img src="./google.png" alt="google" />
           Continue with Google
-        </Link>
+        </button>
       </div>
     </div>
   );
